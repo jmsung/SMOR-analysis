d2ad6b604973155d46bb998c003d0c7f89e70a20
Jongmin Sung <jongmin.sung@roche.com>
2020-03-21 09:36:04 -0700

    use console_scripts

--- setup.py
+++ setup.py
@@ -1,16 +1,31 @@
+
 from setuptools import setup
- 
+
 setup(
-	# This is the name of your PyPI-package
-	name='smb-analysis',    
-
-	# Update the version number for new releases
-	version='0.1',        
-
-	# Specify executables to be installed in the binary folder
-	scripts=['src/smb-analysis.py'],
-
-	install_requires=['numpy==1.17', 'matplotlib==3.1', 'pathlib==1.0', 'scipy==1.3', 'tifffile==0.15', 'imreg_dft==2.0', 'scikit-image==0.15', 'scikit-learn==0.21', 'hmmlearn==0.2.2']
+    name='smb-analysis',
+    version='0.1',
+    description='Single molecule binding analysis',
+    url='https://github.com/jmsung/smb-analysis',
+    author='Jongmin Sung',
+    author_email='jongmin.sung@gmail.com.com',
+    packages=['smb_analysis'],
+    install_requires=[
+		'numpy == 1.17',
+		'matplotlib == 3.1', 
+		'pathlib == 1.0', 
+		'scipy == 1.3', 
+		'tifffile == 0.15', 
+		'imreg_dft == 2.0', 
+		'scikit-image == 0.15', 
+		'scikit-learn == 0.21', 
+		'hmmlearn == 0.2.2'
+        ],
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'smb = smb_analysis.smb:main',
+            ]
+    }
 )
 
 
