02e7bd6921b74f74440658e810116a6915ce8bbb
Jongmin Sung <jongmin.sung@gmail.com>
2021-07-23 15:20:26 -0700

    update required packages

--- setup.py
+++ setup.py
@@ -10,15 +10,15 @@ setup(
     author_email='jongmin.sung@gmail.com.com',
     packages=['smor_analysis'],
     install_requires=[
-        'numpy == 1.17',
-        'matplotlib == 3.1',
-        'pathlib == 1.0',
-        'scipy == 1.5',
-        'tifffile == 0.15',
-        'imreg_dft == 2.0',
-        'scikit-image == 0.15',
-        'scikit-learn == 0.21',
-        'hmmlearn == 0.2.2'
+        'numpy',
+        'matplotlib',
+        'pathlib',
+        'scipy',
+        'tifffile',
+        'imreg_dft',
+        'scikit-image',
+        'scikit-learn',
+        'hmmlearn'
     ],
     include_package_data=True,
     entry_points={
